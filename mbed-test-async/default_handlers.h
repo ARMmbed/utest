44fa1ff46a374c9cb34150badb61b09799bfd61f
Niklas Hauser <niklas.hauser@arm.com>
2015-11-13 14:25:31 +0000

    Document the default handlers.

--- mbed-test-async/default_handlers.h
+++ mbed-test-async/default_handlers.h
@@ -29,6 +29,11 @@ namespace mbed {
 namespace test {
 namespace v0 {
 
+    /** @brief Default handler hint.
+     *
+     * Use this handler to indicate the you want the default handler to be called.
+     * This type automatically casts itself into the appropriate handler type.
+     */
     const struct
     {
         operator test_set_up_handler_t()    const { return test_set_up_handler_t(1); }
@@ -39,6 +44,46 @@ namespace v0 {
         operator case_failure_handler_t()   const { return case_failure_handler_t(1); }
     } default_handler;
 
+    /** @brief Ignore handler hint.
+     *
+     * Use this handler to indicate the you want to ignore this handler and it will not be called.
+     * This type automatically casts itself into the appropriate handler type.
+     */
+    const struct
+    {
+        operator case_handler_t()              const { return case_handler_t(NULL); }
+        operator case_control_flow_handler_t() const { return case_control_flow_handler_t(NULL); }
+
+        operator test_set_up_handler_t()    const { return test_set_up_handler_t(NULL); }
+        operator test_tear_down_handler_t() const { return test_tear_down_handler_t(NULL); }
+
+        operator case_set_up_handler_t()    const { return case_set_up_handler_t(NULL); }
+        operator case_tear_down_handler_t() const { return case_tear_down_handler_t(NULL); }
+        operator case_failure_handler_t()   const { return case_failure_handler_t(NULL); }
+    } ignore_handler;
+
+    /** @brief A table of handlers.
+     *
+     * This structure stores all modifyable handlers and provides accessors to
+     * filter out the default handler.
+     * So if this structure contains handlers, and you want to use these handlers
+     * as a default backup, you can use the `get_handler` function to choose the right handler.
+     *
+     * Example:
+     * @code
+     * const handler_t defaults = { ... }; // your default handlers
+     * // will return the handler in defaults.
+     * test_set_up_handler_t handler = defaults.get_handler(default_handler);
+     * // you will still need to manually check the handler before executing it
+     * if (handler != ignore_handler) handler(...);
+     *
+     * extern test_tear_down_handler_t custom_handler(...);
+     * // will return `custom_handler`
+     * test_tear_down_handler_t handler = defaults.get_handler(custom_handler);
+     * // you will still need to manually check the handler before executing it
+     * if (handler != ignore_handler) handler(...);
+     * @endcode
+     */
     struct handlers_t
     {
         test_set_up_handler_t test_set_up;
@@ -71,13 +116,28 @@ namespace v0 {
         }
     };
 
+    /// Prints the number of tests to run and continues.
     status_t verbose_test_set_up_handler   (const size_t number_of_cases);
+    /// Prints the number of tests that passed and failed with a reason if provided.
     void     verbose_test_tear_down_handler(const size_t passed, const size_t failed, const failure_t failure);
 
+    /// Prints the index and description of the case being run and continues.
     status_t verbose_case_set_up_handler   (const Case *const source, const size_t index_of_case);
+    /// Prints the number of tests that passed and failed with a reason if provided within this case and continues.
     status_t verbose_case_tear_down_handler(const Case *const source, const size_t passed, const size_t failed, const failure_t failure);
+    /// Prints the reason of the failure and continues, unless the teardown handler failed, for which it aborts.
     status_t verbose_case_failure_handler  (const Case *const source, const failure_t reason);
 
+    /// Prints a helpful error message and aborts.
+    /// This function **NEEDS** to be overridden by the user when using greentea.
+    status_t greentea_test_set_up_handler   (const size_t number_of_cases);
+    /// Calls `verbose_test_tear_down_handler` and then prints the greentea failure and success and end strings.
+    void     greentea_test_tear_down_handler(const size_t passed, const size_t failed, const failure_t failure);
+
+    /// Calls `verbose_case_failure_handler` but then aborts.
+    status_t greentea_case_failure_handler  (const Case *const source, const failure_t reason);
+
+    /// The verbose default handlers that always continue on failure
     const handlers_t verbose_continue_handlers = {
         verbose_test_set_up_handler,
         verbose_test_tear_down_handler,
@@ -86,12 +146,7 @@ namespace v0 {
         verbose_case_failure_handler
     };
 
-
-    status_t greentea_test_set_up_handler   (const size_t number_of_cases);
-    void     greentea_test_tear_down_handler(const size_t passed, const size_t failed, const failure_t failure);
-
-    status_t greentea_case_failure_handler  (const Case *const source, const failure_t reason);
-
+    /// The greentea default handlers that always abort on the first encountered failure
     const handlers_t greentea_abort_handlers = {
         greentea_test_set_up_handler,
         greentea_test_tear_down_handler,
@@ -100,6 +155,7 @@ namespace v0 {
         greentea_case_failure_handler
     };
 
+    /// The greentea default handlers that always continue on failure
     const handlers_t greentea_continue_handlers = {
         greentea_test_set_up_handler,
         greentea_test_tear_down_handler,
@@ -108,6 +164,7 @@ namespace v0 {
         verbose_case_failure_handler
     };
 
+    /// The greentea aborting handlers are the default
     const handlers_t default_handlers = greentea_abort_handlers;
 }
 }
