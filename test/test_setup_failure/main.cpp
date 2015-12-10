c35b8a8f1d3bf07174588b49e271abc1ee8e5e58
Niklas Hauser <niklas.hauser@arm.com>
2015-12-10 11:51:12 +0000

    SelfTest: Add specification setup failure.

---
+++ test/test_setup_failure/main.cpp
@@ -0,0 +1,59 @@
+
+/* mbed Microcontroller Library
+ * Copyright (c) 2013-2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "mbed-drivers/mbed.h"
+#include "mbed-drivers/test_env.h"
+#include "utest/utest.h"
+#include "unity/unity.h"
+
+
+using namespace utest::v1;
+
+void test_dummy() {}
+Case cases[] = {
+    Case("dummy test", test_dummy),
+    Case("dummy test 2", test_dummy)
+};
+
+// this setup handler fails
+status_t failing_setup_handler(const size_t number_of_cases) {
+    MBED_HOSTTEST_TIMEOUT(5);
+    MBED_HOSTTEST_SELECT(default_auto);
+    MBED_HOSTTEST_DESCRIPTION(test setup failure test);
+    MBED_HOSTTEST_START("MBED_A1");
+
+    TEST_ASSERT_EQUAL(number_of_cases, 2);
+    verbose_test_setup_handler(number_of_cases);
+    return STATUS_ABORT;    // aborting test
+};
+// the teardown handler will then be called with the reason `FAILURE_SETUP`
+void failing_teardown_handler(const size_t passed, const size_t failed, const failure_t failure) {
+    TEST_ASSERT_EQUAL(passed, 0);
+    TEST_ASSERT_EQUAL(failed, 0);
+    TEST_ASSERT_EQUAL(failure, FAILURE_SETUP);
+
+    verbose_test_teardown_handler(passed, failed, failure);
+
+    if (failure == FAILURE_SETUP) MBED_HOSTTEST_RESULT(true);
+};
+
+Specification specification(failing_setup_handler, cases, failing_teardown_handler);
+
+void app_start(int, char*[]) {
+    static Serial pc(USBTX, USBRX);
+    pc.baud(115200);
+    Harness::run(specification);
+}
