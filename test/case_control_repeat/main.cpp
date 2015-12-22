8e23e940f2bed63acc90bcac43ffb9ab0c95b6c7
Niklas Hauser <niklas.hauser@arm.com>
2015-12-22 11:29:17 +0000

    Selftest: Beautify Control Repeat test.

--- test/case_control_repeat/main.cpp
+++ test/case_control_repeat/main.cpp
@@ -23,83 +23,75 @@ using namespace utest::v1;
 
 int call_counter(0);
 
+// Control: RepeatAll -------------------------------------------------------------------------------------------------
 status_t repeat_all_case_setup(const Case *const source, const size_t index_of_case)
 {
     static int repeat_counter(0);
     TEST_ASSERT_EQUAL(0, index_of_case);
-    TEST_ASSERT_EQUAL(repeat_counter*3, call_counter);
+    TEST_ASSERT_EQUAL(repeat_counter*3, call_counter++);
     repeat_counter++;
-    call_counter++;
     return greentea_case_setup_handler(source, index_of_case);
 }
 control_t repeat_all_case(const size_t call_count)
 {
     printf("Running case handler for %u. time\n", call_count);
     static int repeat_counter(1);
-    TEST_ASSERT_EQUAL(repeat_counter, call_count);
-    repeat_counter++;
-    TEST_ASSERT_EQUAL((call_count-1)*3 + 1, call_counter);
-    call_counter++;
+    TEST_ASSERT_EQUAL(repeat_counter++, call_count);
+    TEST_ASSERT_EQUAL((call_count-1)*3 + 1, call_counter++);
     return (call_count < 10) ? CaseRepeatAll : CaseNoRepeat;
 }
 status_t repeat_all_case_teardown(const Case *const source, const size_t passed, const size_t failed, const failure_t failure)
 {
     static int repeat_counter(0);
-    TEST_ASSERT_EQUAL(repeat_counter*3 + 2, call_counter);
+    TEST_ASSERT_EQUAL(repeat_counter*3 + 2, call_counter++);
     TEST_ASSERT_EQUAL(repeat_counter+1, passed);
     TEST_ASSERT_EQUAL(0, failed);
     TEST_ASSERT_EQUAL(FAILURE_NONE, failure);
     repeat_counter++;
-    call_counter++;
     return greentea_case_teardown_handler(source, passed, failed, failure);
 }
 
+// Control: RepeatHandler ---------------------------------------------------------------------------------------------
 status_t repeat_handler_case_setup(const Case *const source, const size_t index_of_case)
 {
     TEST_ASSERT_EQUAL(1, index_of_case);
-    TEST_ASSERT_EQUAL(30, call_counter);
-    call_counter++;
+    TEST_ASSERT_EQUAL(30, call_counter++);
     return greentea_case_setup_handler(source, index_of_case);
 }
 control_t repeat_handler_case(const size_t call_count)
 {
     printf("Running case handler for %u. time\n", call_count);
     static int repeat_counter(1);
-    TEST_ASSERT_EQUAL(repeat_counter, call_count);
-    repeat_counter++;
-    TEST_ASSERT_EQUAL((call_count-1) + 31, call_counter);
-    call_counter++;
+    TEST_ASSERT_EQUAL(repeat_counter++, call_count);
+    TEST_ASSERT_EQUAL((call_count-1) + 31, call_counter++);
     return (call_count < 10) ? CaseRepeatHandler : CaseNoRepeat;
 }
 status_t repeat_handler_case_teardown(const Case *const source, const size_t passed, const size_t failed, const failure_t failure)
 {
-    TEST_ASSERT_EQUAL(41, call_counter);
+    TEST_ASSERT_EQUAL(41, call_counter++);
     TEST_ASSERT_EQUAL(10, passed);
     TEST_ASSERT_EQUAL(0, failed);
     TEST_ASSERT_EQUAL(FAILURE_NONE, failure);
-    call_counter++;
     return greentea_case_teardown_handler(source, passed, failed, failure);
 }
 
+// Control: NoRepeat --------------------------------------------------------------------------------------------------
 control_t no_repeat_handler_case(const size_t call_count)
 {
     static int repeat_counter(1);
-    TEST_ASSERT_EQUAL(repeat_counter, call_count);
+    TEST_ASSERT_EQUAL(repeat_counter++, call_count);
     TEST_ASSERT_EQUAL(1, call_count);
-    TEST_ASSERT_EQUAL(42, call_counter);
-    repeat_counter++;
-    call_counter++;
+    TEST_ASSERT_EQUAL(42, call_counter++);
     return CaseNoRepeat;
 }
 
+// Control: CaseNext --------------------------------------------------------------------------------------------------
 control_t next_handler_case(const size_t call_count)
 {
     static int repeat_counter(1);
-    TEST_ASSERT_EQUAL(repeat_counter, call_count);
+    TEST_ASSERT_EQUAL(repeat_counter++, call_count);
     TEST_ASSERT_EQUAL(1, call_count);
-    TEST_ASSERT_EQUAL(43, call_counter);
-    repeat_counter++;
-    call_counter++;
+    TEST_ASSERT_EQUAL(43, call_counter++);
     return CaseNext;
 }
 
